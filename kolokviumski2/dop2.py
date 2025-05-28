2e765e61e6b00978cb5bf9b86f8919ac51bbb9d4
Mila Gjurovska <milagjurovska7@gmail.com>
2025-05-28 21:05:00 +0200

    Update dop2.py

--- kolokviumski2/dop2.py
+++ kolokviumski2/dop2.py
@@ -1,31 +1,50 @@
 import os
 
-from sklearn.tree import DecisionTreeClassifier
-
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 #from dataset_script import dataset
 from zad2_dataset_rf import *
+from sklearn.tree import DecisionTreeClassifier
 
 if __name__ == '__main__':
     p=int(input())
-    c=input().strip()
+    train_data=dataset[:int(len(dataset)*p/100)]
+    test_datset=dataset[int(len(dataset)*p/100):]
+    train_x=[row[:-1] for row in train_data]
+    train_y=[row[-1] for row in train_data]
+    test_y=[row[-1] for row in test_datset]
+    test_x=[row[:-1] for row in test_datset]
+
+    c=input()
     l=int(input())
 
-    train_data=dataset[:int(len(dataset)*p/100)]
-    test_data=dataset[int(len(dataset)*p/100):]
+    clf = DecisionTreeClassifier(criterion=c, max_leaf_nodes=l, random_state=0)
+    clf.fit(train_x, train_y)
 
-    X_train, y_train = [row[:-1] for row in train_data], [row[-1] for row in train_data]
-    X_test, y_test = [row[:-1] for row in test_data], [row[-1] for row in test_data]
+    print("Tochnost so originalniot klasifikator:", clf.score(test_x, test_y))
 
-    clf1=DecisionTreeClassifier(criterion=c, max_leaf_nodes=l, random_state=0)
-    clf1.fit(X_train, y_train)
-    print("Tochnost so originalniot klasifikator:", clf1.score(X_test, y_test))
+    modeli=[]
+    for cls in clf.classes_:
+        model = DecisionTreeClassifier(criterion=c, max_leaf_nodes=l, random_state=0)
+        model.fit(train_x, [1 if lab == cls else 0 for lab in train_y])
+        modeli.append((cls,model))
 
-    classificators = []
-    for klasa in ["Perch", "Roach", "Bream"]:
-        clf = DecisionTreeClassifier(criterion=c, max_leaf_nodes=l, random_state=0)
-        clf.fit(X_train, [1 if label == klasa else 0 for label in y_train])
-        classificators.append(clf)
+    acc_count=0
 
-        
+    for r, l in zip(test_x, test_y):
+        prediction=True
+        for c, model in modeli:
+            pred=model.predict([r])[0]
+            if l == c:
+                if pred != 1:
+                    prediction=False
+                    break
+            else:
+                if pred != 0:
+                    prediction=False
+                    break
+
+        if prediction:
+            acc_count+=1
+
+    print("Tochnost so kolekcija od klasifikatori:", acc_count/len(test_datset))
